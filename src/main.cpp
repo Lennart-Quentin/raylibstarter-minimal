0aacd54bb234d551fcc0cc198d78dd5f928f163b
Lennart Grittmann <lennyquen@outlook.de>
2023-04-20 17:03:41 +0200

    first tests

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,26 @@
 #include "config.h"
 
 struct tile {
-    int x, y;
-    int height;
-    int width;
+    float x, y;
+    float height = 1.0;
+    float width = 1.0;
+    int pixels = 32;
 
-    void Draw(){
-        DrawRectangle(x,y,32,32,WHITE);
+    Rectangle GetRect() {
+        return Rectangle{(x - 1) * pixels, (y - 1) * pixels, width * pixels, height * pixels};
+    };
+
+    Vector2 downColPos1() {
+        return Vector2{(x - width) * pixels, y * pixels};
+    }
+
+    Vector2 downColPos2() {
+        return Vector2{x * pixels, y * pixels};
+    }
+
+    void Draw() {
+        DrawRectangle((int) (x - 1) * pixels, (int) (y - 1) * pixels, (int) width * pixels, (int) height * pixels,
+                      WHITE);
     }
 };
 
@@ -32,11 +46,16 @@ int main() {
     Texture2D map = LoadTexture("assets/graphics/test5.png");
 
     tile player;
-    player.x = 0;
-    player.y = 0;
-    player.height = 32;
-    player.width = 32;
+    player.x = 1;
+    player.y = 1;
+    player.height = 1;
+    player.width = 1;
 
+    tile wall;
+    wall.x = 5;
+    wall.y = 2;
+    wall.height = 2;
+    wall.width = 25;
 
 
     // Main game loop
@@ -45,39 +64,45 @@ int main() {
         // Updates that are made by frame are coded here
         // ...
         // ...
-        if(IsKeyPressed(KEY_W)){
-            player.y -= 32;
+        if (IsKeyPressed(KEY_W)) {
+                player.y -= 1;
         }
-        if(IsKeyPressed(KEY_A)){
-            player.x -= 32;
+        if (IsKeyPressed(KEY_A)) {
+            if (!CheckCollisionRecs(player.GetRect(), wall.GetRect())) {
+                player.x -= 1;
+            }
         }
-        if(IsKeyPressed(KEY_S)){
-            player.y += 32;
+        if (IsKeyPressed(KEY_S)) {
+            if (!CheckCollisionRecs(player.GetRect(), wall.GetRect())) {
+                player.y += 1;
+            }
         }
-        if(IsKeyPressed(KEY_D)){
-            player.x += 32;
+        if (IsKeyPressed(KEY_D)) {
+            if (!CheckCollisionRecs(player.GetRect(), wall.GetRect())) {
+                player.x += 1;
+            }
         }
-        if(player.x > 29*32){
-            player.x -= 32;
+        if (player.x > 30) {
+            player.x -= 1;
         }
-        if(player.x < 0){
-            player.x += 32;
+        if (player.x < 1) {
+            player.x += 1;
         }
-        if(player.y > 19*32){
-            player.y -= 32;
+        if (player.y > 20) {
+            player.y -= 1;
         }
-        if(player.y < 0){
-            player.y += 32;
+        if (player.y < 1) {
+            player.y += 1;
         }
 
 
         BeginDrawing();
-            // You can draw on the screen between BeginDrawing() and EndDrawing()
-            // ...
-            // ...
-            ClearBackground(WHITE);
-            DrawTexture(map, 0, 0, WHITE);
-            DrawRectangle(player.x, player.y, 32, 32, WHITE);
+        // You can draw on the screen between BeginDrawing() and EndDrawing()
+        // ...
+        // ...
+        ClearBackground(WHITE);
+        DrawTexture(map, 0, 0, WHITE);
+        player.Draw();
         EndDrawing();
     } // Main game loop end
 
