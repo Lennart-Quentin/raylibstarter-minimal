b3278e44939230f5a1df1ded0345f94931d9538f
Lennart Grittmann <lennyquen@outlook.de>
2023-04-20 09:47:28 +0200

    first tests

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,19 @@
 
 #include "config.h"
 
+struct tile {
+    int x, y;
+    int height;
+    int width;
+
+    void Draw(){
+        DrawRectangle(x,y,32,32,WHITE);
+    }
+};
+
+
 int main() {
+
     // Raylib initialization
     // Project name, screen size, fullscreen mode etc. can be specified in the config.h.in file
     InitWindow(Game::ScreenWidth, Game::ScreenHeight, Game::PROJECT_NAME);
@@ -17,7 +29,15 @@ int main() {
     // Your own initialization code here
     // ...
     // ...
-    Texture2D myTexture = LoadTexture("assets/graphics/testimage.png");
+    Texture2D map = LoadTexture("assets/graphics/test5.png");
+
+    tile player;
+    player.x = 0;
+    player.y = 0;
+    player.height = 32;
+    player.width = 32;
+
+
 
     // Main game loop
     while (!WindowShouldClose()) // Detect window close button or ESC key
@@ -25,22 +45,46 @@ int main() {
         // Updates that are made by frame are coded here
         // ...
         // ...
+        if(IsKeyPressed(KEY_W)){
+            player.y -= 32;
+        }
+        if(IsKeyPressed(KEY_A)){
+            player.x -= 32;
+        }
+        if(IsKeyPressed(KEY_S)){
+            player.y += 32;
+        }
+        if(IsKeyPressed(KEY_D)){
+            player.x += 32;
+        }
+        if(player.x > 29*32){
+            player.x -= 32;
+        }
+        if(player.x < 0){
+            player.x += 32;
+        }
+        if(player.y > 19*32){
+            player.y -= 32;
+        }
+        if(player.y < 0){
+            player.y += 32;
+        }
+
 
         BeginDrawing();
             // You can draw on the screen between BeginDrawing() and EndDrawing()
             // ...
             // ...
             ClearBackground(WHITE);
-            DrawText("Hello, world!", 10, 10, 30, LIGHTGRAY);
-            DrawTexture(myTexture, 10, 100, WHITE);
-
+            DrawTexture(map, 0, 0, WHITE);
+            DrawRectangle(player.x, player.y, 32, 32, WHITE);
         EndDrawing();
     } // Main game loop end
 
     // De-initialization here
     // ...
     // ...
-    UnloadTexture(myTexture);
+    UnloadTexture(map);
 
     // Close window and OpenGL context
     CloseWindow();
